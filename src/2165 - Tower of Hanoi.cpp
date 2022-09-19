11d0b11d1d77c0a7080cbe2c66ee468cc28d0582
DiegoEspinozaA <91444652+DiegoEspinozaA@users.noreply.github.com>
2022-09-19 18:32:30 -0300

    Update 2165 - Tower of Hanoi.cpp

--- src/2165 - Tower of Hanoi.cpp
+++ src/2165 - Tower of Hanoi.cpp
@@ -17,15 +17,8 @@ void solve(int a, int b, int c, int n) {
 	solve(b, a, c, n-1);
 }
 
-signed main(){
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    #ifdef LOCAL
-    freopen("input.txt", "r" , stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    
+main(){
     int n; cin>>n;
     cout<< (1<<n) - 1<<endl;
-
     solve(1, 2, 3, n);
 }
